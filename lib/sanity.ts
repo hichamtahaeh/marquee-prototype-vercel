c2e175b74781dafdaef935e174ea24b5a1688116
Hicham Taha <hichamt@evanshunt.com>
2023-06-22 13:58:00 -0600

    deploy

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -8,7 +8,6 @@ const sanityClient = createClient({
   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.SANITY_API_TOKEN, // Only if you want to update content with the client
 });
-
 module.exports = {
   sanityClient,
 };
